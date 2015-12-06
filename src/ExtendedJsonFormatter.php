46e2a0bd31d1479ed0c10d82c6d01d3f8c45ef67
Maciek W <mwarchalowski@gmail.com>
2015-12-05 21:35:57 -0600

    Initial Commit

---
+++ src/ExtendedJsonFormatter.php
@@ -0,0 +1,38 @@
+<?php
+namespace \Minimax\Monolog;
+use \Monolog\Formatter\JsonFormatter;
+
+/**
+ * Extension to JsonFormatter to inject _SERVER and _REQUEST into context. 
+ *
+ * @author Maciej Warchalowski 
+ */
+class ExtendedJsonFormatter extends JsonFormatter
+{
+    protected function getCustomContext() {
+        $context = $_SERVER;
+        $context['REQUEST_PARAMETERS'] = $_REQUEST;
+        return $context;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function format(array $record)
+    {   
+        $record['context'] = $this->getCustomContext(); 
+        return json_encode($record) . ($this->appendNewline ? "\n" : '');
+    }
+
+    /**
+     * Return a JSON-encoded array of records.
+     *
+     * @param  array  $records
+     * @return string
+     */
+    protected function formatBatchJson(array $records)
+    {   
+        $record['context'] = array_merge($record['context'], $_SERVER); 
+        return json_encode($records);
+    }
+}
